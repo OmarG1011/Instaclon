7b0ea0ce783f554ab400b3a22e13d2b3da3a963c
OmarG1011 <omartega2512@gmail.com>
2025-04-03 23:32:36 -0500

    first commit 3

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserProfile from "../pages/Home";
 import Login from "../pages/Login";
 import SignUp from "../pages/Signup";
-import Profile from '../pages/Profile';  // Importa el nuevo componente de Profile
+import Profile from "../pages/Profile";
 import CreatePost from "../pages/CreatePost";
-
 
 export function MyRoutes() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-            <Route path="/" element={<UserProfile />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/SignUp" element={<SignUp />} />
-            <Route path="/createPost" element={<CreatePost />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
+        <BrowserRouter basename="/Instaclon">
+            <Routes>
+                <Route path="/" element={<UserProfile />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/SignUp" element={<SignUp />} />
+                <Route path="/createPost" element={<CreatePost />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
         </BrowserRouter>
     );
 }
